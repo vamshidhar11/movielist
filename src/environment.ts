053f9277fc7734bac5fbc292bae13e710b9c8aa5
vamshidhar <vamshidhar490@outlook.com>
2018-07-23 18:12:54 +0530

    Prepared to deploy

--- src/environment.ts
+++ src/environment.ts
@@ -1,19 +1,9 @@
-// export default {
-//   debug: true,
-//   testing: true,
-//   apiKey: "8e53e5c8de7b0bd992a1edfe17e50369",
-//   auth0: {
-//     clientId: "hvtFM1lTlxXQh3yiN1eGYmKFqwx5fCns",
-//     domain: "vamshihdhar.auth0.com"
-//   }
-// };
-
 export default {
   debug: true,
   testing: true,
-  apiKey: process.env.API_KEY,
+  apiKey: "8e53e5c8de7b0bd992a1edfe17e50369",
   auth0: {
-    clientId: process.env.CLIENT_ID,
-    domain: process.env.DOMAIN
+    clientId: "hvtFM1lTlxXQh3yiN1eGYmKFqwx5fCns",
+    domain: "vamshihdhar.auth0.com"
   }
 };
